a2a15256a3c6a12137b49a56edbac745d0ed28bf
Derek Dang <21960193+derekdang@users.noreply.github.com>
2023-12-15 23:45:00 -0800

    Day 16 Solutions

--- day16/main.py
+++ day16/main.py
@@ -1,15 +1,12 @@
 # https://adventofcode.com/2023/day/16
 import sys
 from tqdm import tqdm
-# bfs with where each beam is entered into the q
-# if it goes out of bounds
 def p1(entry_point_r, entry_point_c, start_dir):
     card_dirs = {"L": (0, -1), "R": (0, 1), "U": (-1, 0), "D": (1, 0)}
     d_map = {"L": "<", "R": ">", "U": "^", "D": "v"}
-    visited = {}
+    visited = set()
     distinct_dirs = {}
     simple_list = ["<",">", "^", "v"]
-    # chars of interest are "-", "|", "/", "\"
     q = []
     m, n = len(input), len(input[0])
     grid = [["." for _ in range(n)] for _ in range(m)]
@@ -19,11 +16,10 @@ def p1(entry_point_r, entry_point_c, start_dir):
         size = len(q)
         for i in range(size):
             r,c,dir = q.pop(0)
-            # print(len(q))
-            # print(f"row: {r}, col {c}, dir: {dir}")
             if r < 0 or r >= m or c < 0 or c >= n:
                 continue
-            if input[r][c] == ".": # or going in same dir as splitter
+            if input[r][c] == ".": 
+                visited.add((r, c, dir))
                 if grid[r][c] == ".":
                     grid[r][c] = d_map[dir]
                     distinct_dirs[(r,c)] = set()
@@ -34,11 +30,10 @@ def p1(entry_point_r, entry_point_c, start_dir):
                         if len(distinct_dirs[(r,c)]) > 1:
                             grid[r][c] = len(distinct_dirs[(r,c)])
                 new_r, new_c = r + card_dirs[dir][0], c + card_dirs[dir][1]
-                visited[(r, c)] = grid[r][c]
                 q.append((new_r, new_c, dir))
             elif input[r][c] == "/":
                 grid[r][c] = "/"
-                visited[(r, c)] = grid[r][c]
+                visited.add((r, c, dir))
                 new_dir = ""
                 if dir == "R":
                     new_r, new_c, new_dir = go_up(card_dirs, r, c)
@@ -51,7 +46,7 @@ def p1(entry_point_r, entry_point_c, start_dir):
                 q.append((new_r, new_c, new_dir))
             elif input[r][c] == "\\":
                 grid[r][c] = "\\"
-                visited[(r, c)] = grid[r][c]
+                visited.add((r, c, dir))
                 new_dir = ""
                 if dir == "R":
                     new_r, new_c, new_dir = go_down(card_dirs, r, c)
@@ -64,56 +59,52 @@ def p1(entry_point_r, entry_point_c, start_dir):
                 q.append((new_r, new_c, new_dir))
             elif input[r][c] == "-":
                 grid[r][c] = "-"
-                visited[(r, c)] = grid[r][c]
+                visited.add((r, c, dir))
                 if dir == "R" or dir == "L":
                     new_r, new_c = r + card_dirs[dir][0], c + card_dirs[dir][1]
-                    q.append((new_r, new_c, dir))
+                    if (new_r, new_c, dir) not in visited:
+                        q.append((new_r, new_c, dir))
                 elif dir == "D" or dir == "U":
-                    # print(f"splitter {r}, {c}")
                     new_dir = "L"
                     new_r, new_c = r + card_dirs[new_dir][0], c + card_dirs[new_dir][1]
-                    if (new_r, new_c) in visited and visited[(new_r, new_c)] == "|":
+                    if (new_r, new_c, new_dir) in visited:
                         pass
                     else:
                         if (new_r, new_c, new_dir) not in q:
                             q.append((new_r, new_c, new_dir))
                     second_dir = "R"
                     new_r, new_c = r + card_dirs[second_dir][0], c + card_dirs[second_dir][1]
-                    if (new_r, new_c) in visited and visited[(new_r, new_c)] == "|":
+                    if (new_r, new_c, second_dir) in visited:
                         pass
                     else:
                         if (new_r, new_c, second_dir) not in q:
                             q.append((new_r, new_c, second_dir))
             elif input[r][c] == "|":
                 grid[r][c] = "|"
-                visited[(r, c)] = grid[r][c]
+                visited.add((r, c, dir))
                 if dir == "D" or dir == "U":
                     new_r, new_c = r + card_dirs[dir][0], c + card_dirs[dir][1]
-                    q.append((new_r, new_c, dir))
+                    if (new_r, new_c, dir) not in visited:
+                        q.append((new_r, new_c, dir))
                 elif dir == "R" or dir == "L":
-                    # print(f"splitter {r}, {c}")
                     new_dir = "U"
                     new_r, new_c = r + card_dirs[new_dir][0], c + card_dirs[new_dir][1]
-                    if (new_r, new_c) in visited and visited[(new_r, new_c)] == "-":
+                    if (new_r, new_c, new_dir) in visited:
                         pass
                     else:
                         if (new_r, new_c, new_dir) not in q:
                             q.append((new_r, new_c, new_dir))
                     second_dir = "D"
                     new_r, new_c = r + card_dirs[second_dir][0], c + card_dirs[second_dir][1]
-                    if (new_r, new_c) in visited and visited[(new_r, new_c)] == "-":
+                    if (new_r, new_c, second_dir) in visited:
                         pass
                     else:
-                        if (new_r, new_c) not in visited:
+                        if (new_r, new_c, second_dir) not in q:
                             q.append((new_r, new_c, second_dir))
-        # print(q)
-        # if len(visited) == prev_len:
-        #     break
-        # else:
-        #     prev_len = len(visited)
-    # for line in grid:
-    #     print(line)
-    return len(visited)
+    distinct_visits = set()
+    for a,b,_ in visited:
+        distinct_visits.add((a,b))
+    return len(distinct_visits)
             
 def go_left(card_dirs, r, c):
     new_r, new_c = r + card_dirs["L"][0], c + card_dirs["L"][1]
@@ -135,12 +126,9 @@ def go_up(card_dirs, r, c):
     new_dir = "U"   
     return new_r, new_c, new_dir
 
-    
-        
-
 if __name__ == "__main__":
     input = open(sys.argv[1]).read().splitlines()
-    print(p1(0,0,"R"))
+    print(f"Part 1 Number of Tiles Energized: {p1(0,0,'R')}")
     m, n = len(input), len(input[0])
     entry_points = []
     for r in range(m):
@@ -154,7 +142,6 @@ if __name__ == "__main__":
         entry_points.append((m - 1, c, "U"))
 
     biggest_val = 0
-    for e_r, e_c, start_dir in entry_points:
-        print(e_r, e_c, start_dir)
+    for e_r, e_c, start_dir in tqdm(entry_points):
         biggest_val = max(biggest_val, p1(e_r, e_c, start_dir))
-    print(biggest_val)
+    print(f"Part 2 Max Number of Tiles Energized from a specific pt along the edge: {biggest_val}")
